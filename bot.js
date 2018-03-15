972c7c95a5ff727e05e4a1b72001db405e416d6c
Will Czubiak <willczubiak@gmail.com>
2018-03-15 00:33:51 +0000

    added storage of data. Also added love calculator api and a few other test functions

--- bot.js
+++ bot.js
@@ -1,67 +1,106 @@
 var Discord = require('discord.io'),
-    auth = require('./auth.json');
-    
+    auth = require('./auth.json'),
+    unirest = require('unirest'),
+    storage = require('node-persist');
 
 var bot = new Discord.Client({
     token: auth.token,
     autorun: true
 });
 
-var users = [],
-    commands =  'help: Returns all of the commands possible.\n' +
-                'add <username>: Attaches a PUBG username to your discord account.\n' + 
-                'remove <username>: Removes a PUBG username from your discord account.\n' + 
-                'squad <option>: use command "!squad help" for a list of options',
+storage.initSync({
+    continuous: true
+});
+
+var commands =  'help: Returns all of the commands possible.\n' +
+                'squad <option>: Use command "!squad help" for a list of options\n' + 
+                'love <name> <name>: Finds out how compatible two people are.\n' +
+                'pubg set <username>: Attaches your PUBG account to your Discord account.\n' +
+                'pubg reset <username>: Removes your PUBG account from your Discord account.',
     squadCommands = 'list: Displays a list of all squad members.\n' +
                     'add <usernames>: Adds up to 4 usernames (seperate by spaces). This uses your Discord Username.\n' +
                     'remove <username>: Removes username from squad. This uses your Discord Username.\n' +
                     'removeall: Removes everyone from the squad.\n';
-function addUser(userName){
-    users.push(userName);
-    console.log(users);
+                    
+var squad = [];
+function addUser(id, userName){
+    storage.setItemSync(id, userName);
+    console.log(storage.getItemSync(id));
+    console.log(storage.values());
     return true;
 };
 
-function removeUser(username){
-    var index = users.indexOf(username);
-    if (index > -1)
-        users.splice(index, 1); 
-    console.log(users);
+function removeUser(id){
+    storage.removeItemSync(id)
     return true;
+};
+
+function love(fname, sname, id){
+    unirest.get("https://love-calculator.p.mashape.com/getPercentage?fname=" + fname + "&sname=" + sname)
+    .header("X-Mashape-Key", "bbf8SOZNlUmsh4OyTDx1u1cKEofXp1C4ESljsnLTTcOTyWkjKE")
+    .header("Accept", "application/json")
+    .end(function (result) {
+        var loveResponse = "There is a " + result.body.percentage + " percent chance that " + result.body.fname + " and " + result.body.sname + " are compatible.\n" + result.body.result;
+        bot.sendMessage({
+            to: id,
+            message: loveResponse
+        });
+    });
+};
+
+function squadAdd(channelID, args){
+    for (var i = 0; i < args.length; i++){
+        console.log(args[i]);
+    }
 };
 
 bot.on('ready', function() {
     console.log('Logged in as %s - %s\n', bot.username, bot.id);
+    
 });
 
 bot.on('message', function (user, userID, channelID, message, evt) {
-    
+
+    console.log(storage.values());
+    console.log(user);
+
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' '),
-            cmd = args[0];
+            cmd = args[0].toLowerCase(),
+            spot = channelID;
             
             switch(cmd) {
                 case 'help':
-                    response = commands
+                    spot = userID;
+                    response = commands;
                     break;
-                case 'add':
-                    if (addUser(args[1])) {
-                        response = 'Added PUBG user: ' + args[1]
+                case 'pubg':
+                    var success, mode;
+                    if (args.length == 2 || args.length == 3){
+                        if (args[2] != null && args[1] == "set"){
+                            success = addUser(userID, args[2]);
+                            mode = "set";
+                        } else if (args[1] == "reset") {
+                            success = removeUser(userID);
+                            mode = "reset";
+                        } else {
+                            response = 'Invalid use of command. Use "!help" for a list of commands';
+                            break;
+                        }
+                        if (success)
+                            response = 'Succesfully ' + mode + ' your PUBG account.';
+                        else 
+                            response = 'Failed to ' + mode + ' your PUBG account';
                     } else {
-                        response = 'Error adding user. Username is not a PUBG username.'
-                    }
-                    break;
-                case 'remove': 
-                    if (removeUser(args[1])){
-                        response = 'Removed PUBG user: ' + args[1]
-                    } else {
-                        response = 'Error removing user. Username is case sensative.'
+                        response = 'Invalid use of command. Use "!help" for a list of commands';
                     }
                     break;
                 case 'squad':
                     var response = '';
-                    if (args.length < 2)
+                    if (args.length < 2){
                         response = 'Invalid use of !squad. Use "!squad options" for a list of options.\n';
+                        break;
+                    }
                     var opt = args[1];
                     
                     if (opt == 'help'){
@@ -69,7 +108,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     } else if (opt == 'list') {
                         response = 'list command'
                     } else if (opt == 'add'){
-                        response = 'add command';
+                        response = squadAdd(spot, [args.slice(2)]);
                     } else if (opt == 'remove'){
                         response = 'remove command';
                     } else if (opt == 'removeall'){
@@ -79,12 +118,21 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     }
                     
                     break;
+                case 'love':
+                    
+                    if (args.length >= 3){
+                        love(args[1], args[2], spot);
+                    } else {
+                        response = "Invalid use. Please enter two names with the command.";
+                    }
+                    
+                    break;
                 default:
                     response = 'Invalid command. Use "!help" to get a list of commands.';
                     break;
             }
             bot.sendMessage({
-                to: channelID,
+                to: spot,
                 message: response
             });
     }
