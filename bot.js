393e780c54133a47ea50634d3a8b9e016b959e1c
Will <willczubiak@gmail.com>
2020-05-07 20:50:00 -0700

    added logging, and added error handling. As well as new wall of fame for top kills.

--- bot.js
+++ bot.js
@@ -1,15 +1,18 @@
 var Discord = require('discord.js'),
     auth = require('./auth.json'),
     unirest = require('unirest'),
-    AsciiTable = require('ascii-table')
-localStorage = require('node-localstorage').LocalStorage;
+    log = require('log-to-file');
+AsciiTable = require('ascii-table'),
+    localStorage = require('node-localstorage').LocalStorage;
+
 const SCORE = 0;
 const TIME = 1;
 
 var bot = new Discord.Client();
 bot.login(auth.token);
 
-storage = new localStorage('./test');
+storage = new localStorage('./shame');
+top = new localStorage('./top');
 
 var commands = 'help: Returns all of the commands possible.\n' +
     'shame <name>: Shames a person for gettin zero kills\n';
@@ -26,6 +29,8 @@ function score() {
     storage._keys.forEach(person => {
         var item = new Object();
         item.score = getScoreOrTime(storage.getItem(person), SCORE);
+        if (person == "pirate")
+            item.score = item.score / 2;
         item.name = person;
         scoreBoard.push(item);
     });
@@ -38,6 +43,28 @@ function score() {
     response += table.toString();
     response += '```';
 
+    return response;
+}
+
+function topScore() {
+    response = '```\n';
+    scoreBoard = new Array();
+    top._keys.forEach(person => {
+        var item = new Object();
+        item.score = top.getItem(person);
+        item.name = person;
+        if (item.score > 0)
+            scoreBoard.push(item);
+    });
+
+    var table = new AsciiTable('The Fame')
+    scoreBoard.sort(compare);
+    scoreBoard.forEach(function (user) {
+        table.addRow(user.name, user.score);
+    })
+    response += table.toString();
+    response += '```';
+    
     return response;
 }
 
@@ -55,6 +82,9 @@ function getScoreOrTime(str, num) {
 }
 
 function addShame(id, newTime) {
+
+    if (storage._keys.indexOf(id) < 0)
+        return false;
     let shame = getScoreOrTime(storage.getItem(id), SCORE);
     let oldTime = getScoreOrTime(storage.getItem(id), TIME);
     if (newTime > oldTime + 120000)
@@ -62,6 +92,15 @@ function addShame(id, newTime) {
     else
         return false;
     return true;
+}
+
+function topKills(id, kills) {
+    var prevKills = top.getItem(id)
+    if (kills > prevKills) {
+        top.setItem(id, kills);
+        return true;
+    }
+    return false;
 }
 
 function removeShame(id, num) {
@@ -85,79 +124,95 @@ bot.on('ready', function () {
 bot.on('message', message => {
 
     if (!message.guild) return;
-    //console.log(message);
     var emoji;
     var reacting = false;
     var messaging = true;
     var response = '';
 
-    if (message.content.startsWith('!')) {
-        var args = message.content.substring(1).split(' '),
-            cmd = args[0].toLowerCase();
+    try {
+        if (message.content.startsWith('!')) {
+            log(message.author.username + ' ' + message.content + ' ');
 
-        switch (cmd) {
-            case 'help':
-                response = commands;
-                break;
+            var args = message.content.substring(1).split(' '),
+                cmd = args[0].toLowerCase();
 
-            case 'setup':
-                if (message.author.username == 'DCSpud') {
-                    if (addUser(args[1]))
+            switch (cmd) {
+                case 'help':
+                    response = commands;
+                    break;
+
+                case 'setup':
+                    if (message.author.username == 'DCSpud') {
+                        if (addUser(args[1]))
+                            emoji = '✅';
+                        else
+                            emoji = '❌';
+                        reacting = true;
+                        messaging = false;
+
+                    } else {
+                        response = "You don't have permission to do this."
+                    }
+                    break;
+                case 'reset':
+                    if (message.author.username == 'DCSpud') {
+                        storage._keys.forEach(person => {
+                            addUser(person);
+                        });
+                        response = score();
+                    } else {
+                        response = "You don't have permission to do this."
+                    }
+                    break;
+                case 'shame':
+                    if (addShame(args[1], message.createdTimestamp))
                         emoji = '✅';
                     else
                         emoji = '❌';
                     reacting = true;
                     messaging = false;
 
-                } else {
-                    response = "You don't have permission to do this."
-                }
-                break;
-            case 'reset':
-                if (message.author.username == 'DCSpud') {
-                    storage._keys.forEach(person => {
-                        addUser(person);
-                    });
+                    break;
+
+                case 'unshame':
+                    if (message.author.username == 'DCSpud') {
+                        removeShame(args[1], args[2]);
+                        emoji = '✅';
+                        reacting = true;
+                        messaging = false;
+                    } else {
+                        response = "You don't have permission to do this."
+                    }
+                    break;
+                case 'kills':
+                    if (args.length == 3) {
+                        if (topKills(args[1], args[2]))
+                            emoji = '✅';
+                        else 
+                        emoji = '❌';
+                        reacting = true;
+                        messaging = false;
+                    }
+                    break;
+                case 'score':
                     response = score();
-                } else {
-                    response = "You don't have permission to do this."
-                }
-                break;
-            case 'shame':
-                if (addShame(args[1], message.createdTimestamp))
-                    emoji = '✅';
-                else
-                    emoji = '❌';
-                reacting = true;
-                messaging = false;
+                    break;
+                case 'top':
+                    response = topScore();
+                    break;
+                default:
+                    response = 'Invalid command. Use "!help" to get a list of commands.';
+                    break;
+            }
+            if (reacting) {
+                message.react(emoji);
+            }
+            if (messaging && response != '') {
+                message.channel.send(response);
 
-                break;
-
-            case 'unshame':
-                if (message.author.username == 'DCSpud') {
-                    removeShame(args[1], args[2]);
-                    emoji = '✅';
-                    reacting = true;
-                    messaging = false;
-                } else {
-                    response = "You don't have permission to do this."
-                }
-                break;
-
-                break;
-            case 'score':
-                response = score();
-                break;
-            default:
-                response = 'Invalid command. Use "!help" to get a list of commands.';
-                break;
+            }
         }
-        if (reacting) {
-            message.react(emoji);
-        }
-        if (messaging) {
-            message.channel.send(response);
-
-        }
+    } catch {
+        return;
     }
 });
